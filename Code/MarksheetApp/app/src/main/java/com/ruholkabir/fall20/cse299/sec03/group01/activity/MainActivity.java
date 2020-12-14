03837dd873269ce8e506b66fc784ce930beb8591
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-14 22:45:12 +0600

    Documentation #16: Adding home login and main activity java class
    Adding home login and main activity java class.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MainActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ruholkabir.fall20.cse299.sec03.group01;
+package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
 
 public class MainActivity extends AppCompatActivity {
 
