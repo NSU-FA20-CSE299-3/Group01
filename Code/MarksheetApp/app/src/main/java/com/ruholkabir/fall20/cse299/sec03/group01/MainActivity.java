51c938fbe50330366b4199c731ca91c79d057010
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-14 22:31:53 +0600

    Documentation #16 : Re edited the source folder
    Changing the source folder as package name was not on point.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/MainActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/MainActivity.java
@@ -1,16 +1,14 @@
-package com.example.cse299_1721103642.activity;
+package com.ruholkabir.fall20.cse299.sec03.group01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import com.example.cse299_1721103642.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
+        setContentView(R.layout.activity_main);
     }
 }
