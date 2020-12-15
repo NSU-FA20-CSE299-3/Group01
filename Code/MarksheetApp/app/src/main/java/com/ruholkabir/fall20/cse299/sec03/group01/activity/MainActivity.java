c1fcff6e45d0aa574c5fb06010b077d4a47647e0
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-15 23:38:19 +0600

    Enhancement 01 #7 : Adding Buttons
    Ending up with just adding buttons here. I have to create database first else I cannot proceed further.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MainActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MainActivity.java
@@ -3,14 +3,37 @@ package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ruholkabir.fall20.cse299.sec03.group01.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login, register;
+    EditText username, password;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        login=(Button)findViewById(R.id.buttonlogin);
+        register=(Button)findViewById(R.id.buttonregister);
+        username=(EditText)findViewById(R.id.editTextusername);
+        password=(EditText)findViewById(R.id.editTextpassword);
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }
