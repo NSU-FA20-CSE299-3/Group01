9697b00e2aacf15a9e1e4efb88266be1da5dcb47
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-15 23:58:16 +0600

    Enhancement 04 #10 : Created Database
    Database Created Succesfully.

---
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/db/DBAdapter.java
@@ -0,0 +1,17 @@
+package com.ruholkabir.fall20.cse299.sec03.group01.db;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBAdapter extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "Marksheet";
+    private static final String FACULTY_INFO_TABLE = "faculty_table";
+    private static final String STUDENT_INFO_TABLE = "student_table";
+
+
+
+
+
+
+}
