f280096afdcca6c5c1f46d9890a8507346ec8b2f
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-18 17:16:41 +0600

    Enhancement05 #11: Application Context code
    Getter setter of  faculty bean and student bean

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/context/ApplicationContext.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/context/ApplicationContext.java
@@ -1,4 +1,30 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.context;
 
-public class ApplicationContext {
+import android.app.Application;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.bean.FacultyBean;
+import com.ruholkabir.fall20.cse299.sec03.group01.bean.StudentBean;
+
+import java.util.ArrayList;
+
+public class ApplicationContext extends Application {
+
+    private FacultyBean facultyBean;
+    private ArrayList<StudentBean> studentBeanList;
+
+    public FacultyBean getFacultyBean() {
+        return facultyBean;
+    }
+
+    public void setFacultyBean(FacultyBean facultyBean) {
+        this.facultyBean = facultyBean;
+    }
+
+    public ArrayList<StudentBean> getStudentBeanList() {
+        return studentBeanList;
+    }
+
+    public void setStudentBeanList(ArrayList<StudentBean> studentBeanList) {
+        this.studentBeanList = studentBeanList;
+    }
 }
