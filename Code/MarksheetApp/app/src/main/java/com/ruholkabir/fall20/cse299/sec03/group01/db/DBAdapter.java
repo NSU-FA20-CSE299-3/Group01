8b37fdde14c634a193b82a3b37aeaec82fee6007
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-20 00:00:53 +0600

    Enhancemet04 #10: Database
    Working on database, creating table of student and faculty.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/db/DBAdapter.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/db/DBAdapter.java
@@ -1,6 +1,9 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DBAdapter extends SQLiteOpenHelper {
 
@@ -13,9 +16,9 @@ public class DBAdapter extends SQLiteOpenHelper {
     private static final String KEY_FACULTY_FIRSTNAME = "faculty_firstname";
     private static final String KEY_FACULTY_LASTNAME = "faculty_lastname";
     private static final String KEY_FACULTY_MO_NO = "faculty_mobilenumber";
-    private static final String KEY_ADDRESS = "faculty_address";
-    private static final String KEY_USERNAME = "faculty_username";
-    private static final String KEY_PASSWORD = "faculty_password";
+    private static final String KEY_FACULTY_ADDRESS = "faculty_address";
+    private static final String KEY_FACULTY_USERNAME = "faculty_username";
+    private static final String KEY_FACULTY_PASSWORD = "faculty_password";
 
     private static final String KEY_STUDENT_ID = "student_id";
     private static final String KEY_STUDENT_FIRSTNAME = "student_firstname";
@@ -23,9 +26,42 @@ public class DBAdapter extends SQLiteOpenHelper {
     private static final String KEY_STUDENT_MO_NO = "student_mobilenumber";
     private static final String KEY_STUDENT_SECTION = "student_section";
 
+    public DBAdapter(Context context) { super(context, DATABASE_NAME, null, DATABASE_VERSION); }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String queryFaculty="CREATE TABLE "+ FACULTY_INFO_TABLE +" (" +
+                KEY_FACULTY_ID + " INTEGER PRIMARY KEW AUTOINCREAMENT, "+
+                KEY_FACULTY_FIRSTNAME + " TEXT, " +
+                KEY_FACULTY_LASTNAME + " TEXT, "+
+                KEY_STUDENT_MO_NO + " TEXT, "+
+                KEY_FACULTY_ADDRESS + " TEXT, "+
+                KEY_FACULTY_USERNAME + " TEXT, " +
+                KEY_FACULTY_PASSWORD + " TEXT, " + ")";
+        Log.d("queryFaculty", queryFaculty);
+
+
+
+        String queryStudent="CREAT TABLE " + STUDENT_INFO_TABLE + " (" +
+                KEY_STUDENT_ID + " INTEGER, " +
+                KEY_STUDENT_FIRSTNAME + " TEXT, " +
+                KEY_STUDENT_LASTNAME + " TEXT, " +
+                KEY_STUDENT_MO_NO + " TEXT, " +
+                KEY_STUDENT_SECTION + " TEXT, " + ")";
+
+        Log.d("queryStudent", queryStudent );
 
 
 
 
 
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
