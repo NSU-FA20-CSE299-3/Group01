269f516fad9d20b2378c8e3214e93372479f5072
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-24 19:56:32 +0600

    Enhancement 1 #7 : Checking the form box
    Checking that the username or password is Empty or not.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MainActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -23,7 +25,25 @@ public class MainActivity extends AppCompatActivity {
         username=(EditText)findViewById(R.id.editTextusername);
         password=(EditText)findViewById(R.id.editTextpassword);
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user_name = username.getText().toString();
+                String pass_word = password.getText().toString();
+                if(TextUtils.isEmpty(user_name))
+                {
+                    username.setError("Invalid User Name");
+                }
+                else if (TextUtils.isEmpty(pass_word))
+                {
+                    password.setError("Enter Password");
+                }
 
+                
+
+
+            }
+        });
 
 
 
