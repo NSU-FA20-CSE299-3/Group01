93b01319feceb9e9edce253212b388bf7435845c
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-25 16:23:06 +0600

    Enhancement2 #8 : Declaring input variables
    Declaring input variables and set content view to register activity

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/RegisterActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/RegisterActivity.java
@@ -1,6 +1,36 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button registerButton;
+    EditText textFirstName;
+    EditText textLastName;
+    EditText textPhoneNumber;
+    EditText textAddress;
+    EditText textUsername;
+    EditText textPassword;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+
+
+        
+
+    }
+
+
+
+
+
+
 }
