e4c7f46404d8fbf84b73c657c475c78cafa7c800
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-26 19:08:26 +0600

    Enhancement 2 #8 : Checking the register form is empty or not
    Checking the form that any given form is empty or not. If any form left empty then the program will notifiy user to Enter that particular data

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/RegisterActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,8 +43,29 @@ public class RegisterActivity extends AppCompatActivity {
                 String username = textUsername.getText().toString();
                 String password = textPassword.getText().toString();
                 
-
-
+                if(TextUtils.isEmpty(first_name)){
+                    textFirstName.setError("Please Enter YOUR first name");
+                }
+                else if(TextUtils.isEmpty(last_name))
+                {
+                    textLastName.setError("Please Enter YOUR last name");
+                }
+                else if(TextUtils.isEmpty(phone_number)){
+                    textPhoneNumber.setError("Please Enter YOUR phone number");
+                }
+                else if(TextUtils.isEmpty(address))
+                {
+                    textAddress.setError("Please Enter YOUR address");
+                }
+                else if(TextUtils.isEmpty(username))
+                {
+                    textUsername.setError("Please Enter a Username");
+                }
+                else if(TextUtils.isEmpty(password))
+                {
+                    textPassword.setError("Please Enter a password");
+                }
+                
             }
         });
 
