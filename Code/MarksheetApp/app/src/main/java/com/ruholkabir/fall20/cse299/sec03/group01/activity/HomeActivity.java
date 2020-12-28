6479a3184f9c0745c0b53917719d4e53357a0bce
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2020-12-28 19:44:13 +0600

    Enhancement 03 #9 : Decalring Home activity buttons
    Decalring Home activity buttons and setting content view to home.xml

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/HomeActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/HomeActivity.java
@@ -1,6 +1,32 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
+    Button addStudent;
+    Button StudentProgress;
+    Button viewStudent;
+    Button marksheet;
+    Button examMarks;
+    Button logout;
+
+    Protected void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.home);
+
+
+        addStudent = (Button)findViewById(R.id.buttonAddStudent);
+        StudentProgress= (Button)findViewById(R.id.buttonStudentProgress);
+        viewStudent = (Button)findViewById(R.id.buttonViewStudent);
+        marksheet = (Button)findViewById(R.id.buttonMarksheet);
+        examMarks = (Button)findViewById(R.id.buttonExamMarks);
+        logout = (Button)findViewById(R.id.buttonLogout);
+
+    }
+
 }
