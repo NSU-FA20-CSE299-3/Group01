1f0143a20c87fae8c99e4f3dd918c2d09757e7da
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2021-01-03 13:54:53 +0600

    Enhancement 08 #19 : Defining button and edit text variable
    Defining button and edit text variable and binding add_student .xml file with add student .java file

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/AddStudentActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/AddStudentActivity.java
@@ -1,6 +1,26 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddStudentActivity extends AppCompatActivity {
+
+    Button addbutton;
+    EditText textID;
+    EditText textFirstName;
+    EditText textLastName;
+    EditText textMobileNumber;
+    EditText textSection;
+
+
+    protected void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.add_student);
+    }
+
 }
