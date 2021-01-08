04cb99f4668a4abcefb722343b555107f8572f03
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2021-01-08 18:22:04 +0600

    Enhancement 11 #24 : Declared variables and set content view
    Declared variables and set content view. Connected view student xml file with .java file

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/ViewStudent.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/ViewStudent.java
@@ -1,6 +1,26 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewStudent extends AppCompatActivity {
+    Spinner spinnerSection;
+    String section;
+    private String[] sectionString = new String[]{"1","2","3","4","5"};
+    Button submit;
+
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewstudent);
+
+        
+
+    }
+
 }
