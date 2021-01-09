82df17099b02f921f161e82b7f945cf404253ca7
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2021-01-09 20:42:49 +0600

    Enhancement 11 #24 : Made some mistake. will sort out later
    I coded to print the student list. maybe I made some mistake. i will find out it later.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/StudentListView.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/StudentListView.java
@@ -1,6 +1,7 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,5 +24,45 @@ public class StudentListView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.studentlistview);
+
+        listView = (ListView)findViewById(R.id.ListViewofStudent);
+        final Arraylist<String> studentList = new ArrayList<>();
+        section = getIntent().getExtras().getString("section");
+
+        studentBeanList = dbAdapter.getAllStudentBySection(section);
+        for(StudentBean studentBean : studentBeanList);
+        {
+            String users = studentBean.getStudent_id + "     "+studentBean.getStudent_firstname()+"     "+studentBean.getStudent_lastname()+"     "+studentBean.getStudent_mobilenumber();
+            studentList.add(users);
+            Log.d("users: ", users);
+        }
+
+        listAdapter = new ArrayAdapter<String>(this, R.layout.view_student_list,R.id.label, studentList);
+        listView.setAdapter(listAdapter);
+
+        listView.setOnItemLongClickListener(new adapterView.OnItemLongClickListener());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
