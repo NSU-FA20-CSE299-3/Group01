64e13aa218e7efdb7eed6333d9019a17a78754ea
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2021-01-09 20:25:34 +0600

    Enhancement 11 #24 : Connecting database and declaring variable
    Connecting database and declaring variable. And showing content view.

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/StudentListView.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/StudentListView.java
@@ -1,6 +1,27 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
+import com.ruholkabir.fall20.cse299.sec03.group01.bean.StudentBean;
+import com.ruholkabir.fall20.cse299.sec03.group01.db.DBAdapter;
+
+import java.util.ArrayList;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentListView extends AppCompatActivity {
+    ArrayList<StudentBean> studentBeanList;
+    private ListView listView;
+    private ArrayAdapter<String> listAdapter;
+    String section;
+
+    DBAdapter dbAdapter = new DBAdapter(this);
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.studentlistview);
+    }
 }
