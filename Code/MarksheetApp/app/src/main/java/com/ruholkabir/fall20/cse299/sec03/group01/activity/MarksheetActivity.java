704377a7bd12a1924fdd52cb9804db15e39c9fc3
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2021-01-11 19:03:52 +0600

    Enhancement 12 #31 : Set content view
    Connecting .xml file with .java file

--- Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MarksheetActivity.java
+++ Code/MarksheetApp/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/group01/activity/MarksheetActivity.java
@@ -1,6 +1,16 @@
 package com.ruholkabir.fall20.cse299.sec03.group01.activity;
 
+import android.os.Bundle;
+
+import com.ruholkabir.fall20.cse299.sec03.group01.R;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MarksheetActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.marksheet);
+    }
+
 }
