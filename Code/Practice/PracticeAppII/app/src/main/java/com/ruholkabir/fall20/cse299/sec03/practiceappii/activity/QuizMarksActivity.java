96a3a9e88f100ee99a39a40c6f75646cfc4d5bcc
KaziRuholKabir <kazi.moahhed@northsouth.edu>
2021-01-15 00:13:07 +0600

    Enhancement 15 #34 : Quiz marks Activity complete
    set the data to the database from this activity

--- Code/Practice/PracticeAppII/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/practiceappii/activity/QuizMarksActivity.java
+++ Code/Practice/PracticeAppII/app/src/main/java/com/ruholkabir/fall20/cse299/sec03/practiceappii/activity/QuizMarksActivity.java
@@ -1,12 +1,16 @@
 package com.ruholkabir.fall20.cse299.sec03.practiceappii.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.ruholkabir.fall20.cse299.sec03.practiceappii.R;
+import com.ruholkabir.fall20.cse299.sec03.practiceappii.bean.QuizBean;
+import com.ruholkabir.fall20.cse299.sec03.practiceappii.db.DBAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -55,6 +59,30 @@ public class QuizMarksActivity extends AppCompatActivity {
                 else if(TextUtils.isEmpty(quiz_marks)){
                     textQuizMarks.setError("Enter Quiz Marks");
                 }
+                else{
+                    QuizBean quizBean = new QuizBean();
+                    quizBean.setStudent_id_quiz(student_id_quiz);
+                    quizBean.setStudent_name_quiz(student_name_quiz);
+                    quizBean.setStudent_section_quiz(student_section_quiz);
+                    quizBean.setStudent_quiz_marks(quiz_marks);
+
+
+                    DBAdapter dbAdapter = new DBAdapter(QuizMarksActivity.this);
+                    dbAdapter.addQuizMarks(quizBean);
+                    Intent intent = new Intent(QuizMarksActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "Quiz Marks added Succesfully", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+
+
+
+
+
+
             }
         });
 
